11d1d4256fe5e2eb43d80a5f32ee12e9c1cd457c
Matthew Parsons <mintidus@gmail.com>
2020-07-25 13:03:19 -0400

    added back the generate button and write password function that was provided when starting the project

--- script.js
+++ script.js
@@ -76,3 +76,19 @@ debugger;
 console.log(result);
   return result;
 }
+
+  // Assignment Code
+  var generateBtn = document.querySelector("#generate");
+  
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+  }
+
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+  
